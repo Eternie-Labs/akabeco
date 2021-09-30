03f270f57190d697a9d6ddd77db2485a6f5ffb1a
Hirochika Yuda <hyuda@outlook.jp>
2021-09-30 20:09:32 +0900

    Removing headers which seems not to be working well

--- next.config.js
+++ next.config.js
@@ -1,35 +1,8 @@
-const securityHeaders = [
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  {
-    key: 'Permissions-Policy',
-    value: 'interest-cohort=()'
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff'
-  },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains; preload'
-  }
-]
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   i18n: {
     locales: ['ja', 'en'],
     defaultLocale: 'ja'
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      }
-    ]
   },
 }
